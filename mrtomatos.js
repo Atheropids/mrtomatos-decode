48a063f99c7d69acd150b3159e6e014be86aa6b6
Atheropids <84617941+Atheropids@users.noreply.github.com>
2022-02-26 03:46:13 -0800

    Update mrtomatos.js

--- mrtomatos.js
+++ mrtomatos.js
@@ -4,9 +4,13 @@ const fs = require('fs');
 
 let workspace = 'D:/workspace';
 let lines = fs.readFileSync(`${workspace}/USELESS TEXT.txt`).toString('utf16le').slice(1).split(/\r?\n/g);
+
+// Key for the decoding process.
+let key = 23;
+
 let x = 0;
 let str_in = '';
-let key = 23;
+
 for(let i = 0 ; i < lines.length ; i++)
 {
   if(lines[i].length >= x)
