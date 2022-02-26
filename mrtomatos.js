fcd9d2e76bbc2a3adbbd6c6fc79ddcb363f5bad6
Atheropids <84617941+Atheropids@users.noreply.github.com>
2022-02-26 03:40:49 -0800

    Create mrtomatos.js

---
+++ mrtomatos.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs');
+
+let workspace = 'D:/InstaQuick/workspace/temp';
+let lines = fs.readFileSync(`${workspace}/USELESS TEXT.txt`).toString('utf16le').slice(1).split(/\r?\n/g);
+let x = 0;
+let str_in = '';
+let key = 23;
+for(let i = 0 ; i < lines.length ; i++)
+{
+  if(lines[i].length >= x)
+  {
+    x = lines[i].length;
+    str_in += lines[i];
+  }
+  else
+  {
+    str_in += lines[i].slice(0, -5);
+
+    let code = lines[i].slice(-5);
+    let str = '';
+
+    for(let j = 0 ; j < str_in.length ; j += 2)
+    {
+      str += String.fromCharCode(parseInt(str_in.slice(j, j + 2)) + key);
+    }
+
+    str += code;
+    console.log(decodeURIComponent(str).replace(/\+/g, ' ').replace('<br/>', '\n'));
+  }
+}
